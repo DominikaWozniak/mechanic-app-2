df15f8a3bb82837c1b6ba877e6cc2b4dcf77f2a6
domin <dominika.github@gmail.com>
2021-05-02 13:59:01 +0200

    implemented method in services

--- src/main/java/pl/project/mechanicapp2/services/impl/CarOwnerServiceImpl.java
+++ src/main/java/pl/project/mechanicapp2/services/impl/CarOwnerServiceImpl.java
@@ -2,29 +2,45 @@ package pl.project.mechanicapp2.services.impl;
 
 import org.springframework.stereotype.Service;
 import pl.project.mechanicapp2.model.CarOwner;
+import pl.project.mechanicapp2.repositories.CarOwnerRepository;
 import pl.project.mechanicapp2.services.CarOwnerService;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CarOwnerServiceImpl implements CarOwnerService {
+
+    private CarOwnerRepository carOwnerRepository;
+
+    public CarOwnerServiceImpl(CarOwnerRepository carOwnerRepository) {
+        this.carOwnerRepository = carOwnerRepository;
+    }
+
     @Override
     public List<CarOwner> findAllItems() {
-        return null;
+        return carOwnerRepository.findAll();
     }
 
     @Override
     public void saveItem(CarOwner item) {
-
+        this.carOwnerRepository.save(item);
     }
 
     @Override
     public CarOwner getItemById(Long id) {
-        return null;
+        Optional<CarOwner> optional = carOwnerRepository.findById(id);
+        CarOwner carOwner = null;
+        if (optional.isPresent()) {
+            carOwner = optional.get();
+        } else {
+            throw new RuntimeException(" Owner not found for id :: " + id);
+        }
+        return carOwner;
     }
 
     @Override
     public void deleteItemById(Long id) {
-
+        this.carOwnerRepository.deleteById(id);
     }
 }
--- src/main/java/pl/project/mechanicapp2/services/impl/CarServiceImpl.java
+++ src/main/java/pl/project/mechanicapp2/services/impl/CarServiceImpl.java
@@ -2,29 +2,45 @@ package pl.project.mechanicapp2.services.impl;
 
 import org.springframework.stereotype.Service;
 import pl.project.mechanicapp2.model.Car;
+import pl.project.mechanicapp2.repositories.CarRepository;
 import pl.project.mechanicapp2.services.CarService;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CarServiceImpl implements CarService {
+
+    private CarRepository carRepository;
+
+    public CarServiceImpl(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
     @Override
     public List<Car> findAllItems() {
-        return null;
+        return carRepository.findAll();
     }
 
     @Override
     public void saveItem(Car item) {
-
+        this.carRepository.save(item);
     }
 
     @Override
     public Car getItemById(Long id) {
-        return null;
+        Optional<Car> optional = carRepository.findById(id);
+        Car car = null;
+        if (optional.isPresent()) {
+            car = optional.get();
+        } else {
+            throw new RuntimeException(" Car not found for id :: " + id);
+        }
+        return car;
     }
 
     @Override
     public void deleteItemById(Long id) {
-
+        this.carRepository.deleteById(id);
     }
 }
