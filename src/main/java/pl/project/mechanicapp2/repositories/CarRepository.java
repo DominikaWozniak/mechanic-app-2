86c9cdd7a2c2797568e7be3961f2a30af035160a
Dominika Woźniak <dominika.github@gmail.com>
2021-05-03 13:30:50 +0200

    added new query to CarRepository.java

--- src/main/java/pl/project/mechanicapp2/repositories/CarRepository.java
+++ src/main/java/pl/project/mechanicapp2/repositories/CarRepository.java
@@ -1,7 +1,15 @@
 package pl.project.mechanicapp2.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.project.mechanicapp2.model.Car;
 
+import java.util.List;
+
 public interface CarRepository extends JpaRepository<Car, Long> {
+
+    @Query(value = "SELECT * FROM car c WHERE c.car_owner_id = :id",
+            nativeQuery = true)
+    List<Car> findCarByOwnerId(@Param("id") Long id);
 }
