68e55bd42b6fa4b31f0d2a194ef6c379e5540e17
Dominika Woźniak <dominika.github@gmail.com>
2021-05-03 13:33:33 +0200

    added new methods to CarOwnerController

--- src/main/java/pl/project/mechanicapp2/controller/CarOwnerController.java
+++ src/main/java/pl/project/mechanicapp2/controller/CarOwnerController.java
@@ -3,23 +3,47 @@ package pl.project.mechanicapp2.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.project.mechanicapp2.model.Car;
 import pl.project.mechanicapp2.model.CarOwner;
 import pl.project.mechanicapp2.services.CarOwnerService;
+import pl.project.mechanicapp2.services.CarService;
 
 @Controller
 public class CarOwnerController {
 
     private CarOwnerService carOwnerService;
+    private CarService carService;
 
-    public CarOwnerController(CarOwnerService carOwnerService) {
+    public CarOwnerController(CarOwnerService carOwnerService, CarService carService) {
         this.carOwnerService = carOwnerService;
+        this.carService = carService;
     }
 
-    @GetMapping("/showOwnerThisCar/{id}")
-    public String viewThisOwner(@PathVariable("id") Long id, Model model){
-        CarOwner carOwner = carOwnerService.getItemById(id);
-        model.addAttribute("carOwner", carOwner);
-        return "owner_this_car";
+    @GetMapping("/carOwnerList")
+    public String viewOwnerList(Model model){
+        model.addAttribute("carOwners", carOwnerService.findAllItems());
+        return "car_owner_list";
+    }
+
+    @GetMapping("/selectCars/{id}")
+    public String selectCars(@PathVariable("id") Long id, Model model){
+        model.addAttribute("carsByOwnerId", carService.findCarByOwnerId(id));
+        return "selected_cars";
+    }
+
+    @GetMapping("/carForm/{id}")
+    public String newCarForm(@PathVariable("id") Long id, Model model){
+        Car car = new Car();
+        model.addAttribute("newCar", car);
+        return "new_car";
+    }
+
+    @PostMapping("/saveCar")
+    public String saveCar(@ModelAttribute("newCar") Car car){
+        carService.saveItem(car);
+        return "redirect:/carOwnerList";
     }
 }
