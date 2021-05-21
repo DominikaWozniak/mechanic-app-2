07b8d4a7784a30257c44c15a32825e2277e3c59c
domin <dominika.github@gmail.com>
2021-05-21 16:30:57 +0200

    added new method to repository and service

--- src/main/java/pl/project/mechanicapp2/repositories/AddressRepository.java
+++ src/main/java/pl/project/mechanicapp2/repositories/AddressRepository.java
@@ -1,9 +1,16 @@
 package pl.project.mechanicapp2.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.project.mechanicapp2.model.Address;
+
+import java.util.List;
 
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
+    @Query(value = "SELECT * FROM address a WHERE a.car_owner_id = :id",
+            nativeQuery = true)
+    List<Address> findAddressByOwnerId(@Param("id") Long id);
 }
