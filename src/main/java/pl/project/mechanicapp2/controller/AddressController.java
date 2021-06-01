0063cc8126b662a8ac07c15326829163c579d35d
domin <dominika.github@gmail.com>
2021-06-01 15:34:59 +0200

    removed viewAddress method

--- src/main/java/pl/project/mechanicapp2/controller/AddressController.java
+++ src/main/java/pl/project/mechanicapp2/controller/AddressController.java
@@ -1,8 +1,6 @@
 package pl.project.mechanicapp2.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import pl.project.mechanicapp2.services.AddressService;
 
 @Controller
@@ -14,10 +12,5 @@ public class AddressController {
         this.addressService = addressService;
     }
 
-    @GetMapping("/address")
-    public String viewAddress(Model model) {
-        model.addAttribute("addressesList", addressService.findAllItems());
-        return "addresses";
-    }
 
 }
